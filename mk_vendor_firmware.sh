284847b3e2009e6883df96827249971d10f8f0c2
merothh <merothh@gmail.com>
2019-05-16 20:45:01 +0530

    mk_vendor_firmware: assert each function
    * if any command fails, gtfo

--- mk_vendor_firmware.sh
+++ mk_vendor_firmware.sh
@@ -1,7 +1,15 @@
+assert(){
+if [ "$?" -ne "0" ]; then
+    echo -e "\nsub-process ${FUNCNAME[1]} failed. Bailing out!"
+    exit
+fi
+}
+
 unzip_ota(){
 rm -rf .workspace
 mkdir .workspace
 unzip $1 vendor.new.dat.br vendor.transfer.list vendor.patch.dat -d .workspace
+assert
 }
 
 if [ -z "$1" ]; then
