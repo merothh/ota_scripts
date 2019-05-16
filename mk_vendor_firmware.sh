8038c901de100961568f24519898439b0d453d89
merothh <merothh@gmail.com>
2019-05-16 21:08:38 +0530

    mk_vendor_firmware: tab out stuff

--- mk_vendor_firmware.sh
+++ mk_vendor_firmware.sh
@@ -4,18 +4,18 @@ red='tput setaf 1'
 reset='tput sgr0'
 
 assert(){
-if [ "$?" -ne "0" ]; then
-    echo -e "\n$($red)// Sub-Process $($yellow)${FUNCNAME[1]}$($red) failed. Bailing out!$($reset)"
-    exit
-fi
+    if [ "$?" -ne "0" ]; then
+        echo -e "\n$($red)// Sub-Process $($yellow)${FUNCNAME[1]}$($red) failed. Bailing out!$($reset)"
+        exit
+    fi
 }
 
 unzip_ota(){
-echo -e "\n$($cyan)// Unzipping $($yellow)$1$($reset)\n"
-rm -rf .workspace
-mkdir .workspace
-unzip $1 vendor.new.dat.br vendor.transfer.list vendor.patch.dat -d .workspace
-assert
+    echo -e "\n$($cyan)// Unzipping $($yellow)$1$($reset)\n"
+    rm -rf .workspace
+    mkdir .workspace
+    unzip $1 vendor.new.dat.br vendor.transfer.list vendor.patch.dat -d .workspace
+    assert
 }
 
 if [ -z "$1" ]; then
